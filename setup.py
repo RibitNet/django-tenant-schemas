ef0ad89dc518a698a76b640925be397640b1c985
Simon Coulton <simon.coulton@gmail.com>
2019-02-21 11:46:20 +1100

    chore: reverting back

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='ribit-django-tenant-schemas',
+    name='django-tenant-schemas',
     version=get_git_version(),
     author='Bernardo Pires Carneiro',
     author_email='carneiro.be@gmail.com',
